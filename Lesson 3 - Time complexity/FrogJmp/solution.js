b9d602c1ef10ba86c5704e7e81eb738653942add
kalkusa <kalkus@10g.pl>
2020-02-27 23:27:38 +0100

    FrogJmp proper solution

--- Lesson 3 - Time complexity/FrogJmp/solution.js
+++ Lesson 3 - Time complexity/FrogJmp/solution.js
@@ -2,14 +2,8 @@ let solution = (x, y, d) => {
   let initialPosition = x
   let finalPosition = y
   let jumpDistance = d
-  let currentPosition = initialPosition
-  let jumps = 0
 
-  while (currentPosition < finalPosition) {
-    jumps++
-    currentPosition += jumpDistance
-  }
-  return jumps
+  return Math.ceil((finalPosition - initialPosition) / jumpDistance)
 }
 
 module.exports = solution

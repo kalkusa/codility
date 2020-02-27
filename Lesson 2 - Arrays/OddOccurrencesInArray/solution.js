e0db6ec0de8c5047fda88d3eaef01cb3819c5030
kalkusa <kalkus@10g.pl>
2020-02-27 22:33:31 +0100

    100 percent solution of OddOccurrencesInArray

--- Lesson 2 - Arrays/OddOccurrencesInArray/solution.js
+++ Lesson 2 - Arrays/OddOccurrencesInArray/solution.js
@@ -1,22 +1,31 @@
 let solution = arg => {
-  let occurrencesDictionary = []
-  let oddCandidatesList = []
-  for (let i = 0; i < arg.length; i++) {
-    let currentNumber = arg[i]
-    if (occurrencesDictionary[currentNumber]) {
-      occurrencesDictionary[currentNumber]++
-      if (occurrencesDictionary[currentNumber] % 2 == 1) {
-        oddCandidatesList.push(currentNumber)
-      } else {
-        oddCandidatesList.splice(oddCandidatesList.indexOf(currentNumber), 1)
-      }
-    } else {
-      occurrencesDictionary[currentNumber] = 1
-      oddCandidatesList.push(currentNumber)
-    }
+  if (arg.length === 1) {
+    return arg[0]
   }
 
-  return oddCandidatesList[0]
+  let sorted = arg.sort()
+  let i = 1
+  let previous = sorted[0]
+  let repetitionCount = 1
+  while (i <= sorted.length) {
+    let current = sorted[i]
+    if (previous === current) {
+      repetitionCount++
+    } else {
+      if (repetitionCount % 2 === 1) {
+        //We found odd repetition so one element doesn't have a pair
+        return previous
+      } else {
+        repetitionCount = 1
+      }
+    }
+    if (i + 1 === sorted.length && repetitionCount % 2 === 1) {
+      //We are at the last element of the array and repetition is odd - so current element doesn't have a pair
+      return current
+    }
+    i++
+    previous = current
+  }
 }
 
 module.exports = solution

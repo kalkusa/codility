19dc598b060abae5faa6ed31a2dc819c9427d087
kalkusa <kalkus@10g.pl>
2020-02-27 21:59:23 +0100

    Second 77 percent well solution

--- Lesson 2 - Arrays/OddOccurrencesInArray/solution.js
+++ Lesson 2 - Arrays/OddOccurrencesInArray/solution.js
@@ -1,22 +1,22 @@
 let solution = arg => {
   let occurrencesDictionary = []
+  let oddCandidatesList = []
   for (let i = 0; i < arg.length; i++) {
-    if (occurrencesDictionary[arg[i]]) {
-      occurrencesDictionary[arg[i]]++
+    let currentNumber = arg[i]
+    if (occurrencesDictionary[currentNumber]) {
+      occurrencesDictionary[currentNumber]++
+      if (occurrencesDictionary[currentNumber] % 2 == 1) {
+        oddCandidatesList.push(currentNumber)
+      } else {
+        oddCandidatesList.splice(oddCandidatesList.indexOf(currentNumber), 1)
+      }
     } else {
-      occurrencesDictionary[arg[i]] = 1
+      occurrencesDictionary[currentNumber] = 1
+      oddCandidatesList.push(currentNumber)
     }
   }
 
-  let result = 0
-  occurrencesDictionary.forEach((value, key) => {
-    if (value % 2 == 1) {
-      result = key
-      return
-    }
-  })
-
-  return result
+  return oddCandidatesList[0]
 }
 
 module.exports = solution
--- Lesson 2 - Arrays/OddOccurrencesInArray/test.js
+++ Lesson 2 - Arrays/OddOccurrencesInArray/test.js
@@ -4,3 +4,8 @@ test('[9, 3, 9, 3, 9, 7, 9] should return 7', () => {
   let array = [9, 3, 9, 3, 9, 7, 9]
   expect(solution(array)).toBe(7)
 })
+
+test('[100, 3, 100, 3, 9, 7, 9, 7, 15, 4, 4, 4, 4] should return 15', () => {
+  let array = [100, 3, 100, 3, 9, 7, 9, 7, 15, 4, 4, 4, 4]
+  expect(solution(array)).toBe(15)
+})
